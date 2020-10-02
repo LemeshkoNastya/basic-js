2c935e86cb06e2105a32510d3cf05ef381a1a4da
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2020-10-02 17:21:48 +0400

    feat: add solition recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr) {
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if (!Array.isArray(arr)) {
+      return 0;
+    } else {
+      let depth = 1;
+      for ( let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          let depthElement = 1;
+          depthElement += this.calculateDepth(arr[i]);
+          if (depthElement > depth) {
+            depth = depthElement;
+          }
+        }
+      }
+      return depth;
+    }
   }
 };
